a3d6839050553e8caa37671c572161e7df62cc9a
ratlord <bennett.daniel@gmail.com>
2022-12-10 16:33:36 -0500

    scrollspy styling complete

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -11,9 +11,7 @@ import { Observable } from 'rxjs';
   styleUrls: ['./intro.component.scss']
 })
 export class IntroComponent implements OnInit {
-  @ViewChild('sec_circle_1', {static: true}) sec_circle_1: ElementRef;
-  testClass = 'paragraph-circle'
-
+  _mapActive:boolean;
   scrolledTargets: string[] = [];
 
   spyClass = {
@@ -44,9 +42,6 @@ export class IntroComponent implements OnInit {
     p_circle_8_3:'paragraph-circle',
   }
 
-
-  _mapActive:boolean;
-
   constructor(private store: Store) { }
 
   ngOnInit(): void {
@@ -61,54 +56,36 @@ export class IntroComponent implements OnInit {
     })
   }
 
-
-
+  // Scrollspy interaction
   onIntersection(event) {
     let eventTarget = event[0].target.id;
-    let className = event[0].target.className;
     let inView = event[0].isIntersecting;
     let ratio = event[0].intersectionRatio;
     let boundY = event[0].boundingClientRect.y
     let targetStatus = [eventTarget, inView]
-    // console.log(targetStatus);
-
-
+    let yLag = boundY + 200;
+    console.log(event[0]);
 
     /**
-     * ADD TO ARRAY - GOOD
+     * ADD TO ARRAY
      * when intersection target comes into view, the circle id is pushed to array for tracking,
      * and those in array will have css class changed to active below
      */
     if (inView && !this.scrolledTargets.includes(targetStatus[0])) {
       this.scrolledTargets.push(targetStatus[0]);
-      if (eventTarget === "sec_circle_2") {
-        console.log('adding');
-
-        console.log(event[0]);
-
-      }
-      console.log(this.scrolledTargets);
     }
 
     /**
-     * PROBLEM - RANDOMLY REMOVING FROM ARRAY - FIXED?!
-     * REMOVE FROM ARRAY - GOOD
+     * REMOVE FROM ARRAY
      * when intersection target out of view because user has scrolled above target, the circle id
      * is removed from array so will have css class changed to (in)active
      */
     if (!inView && boundY > 100) {
       let newArray = this.scrolledTargets.filter((item) => item !== eventTarget);
       this.scrolledTargets = newArray
-      if (eventTarget === "sec_circle_2") {
-        console.log('removing');
-        console.log(event[0]);
-
-      }
-      console.log(this.scrolledTargets);
-
     }
 
-    // Change class to +"-active" when included array - GOOD
+    // Change class to +"-active" when included array
     this.scrolledTargets.forEach((target) => {
       const tLength = target.length;
       const tValue = this.spyClass[`${target}`];
@@ -123,73 +100,24 @@ export class IntroComponent implements OnInit {
       }
     })
 
+
   //  Change class to  remove "-active" when target excluded from array - BAD
     this.scrolledTargets.forEach((target) => {
-      // const tLength = target.length;
-      let tValue = this.spyClass[`${eventTarget}`];
+      const inactiveSection = 'section-circle';
+      const inactiveParagraph = 'paragraph-circle'
 
-      if (!this.scrolledTargets.includes(eventTarget)) {
-        console.log('SHIT');
-        // console.log(tLength);
-        console.log(eventTarget);
+      if (!this.scrolledTargets.includes(eventTarget) &&
+          this.spyClass[`${eventTarget}`] ==='section-circle-active' ) {
 
-        console.log(tValue);
-        // removal is registered, now need to remove "-active" from class
-        console.log(this.spyClass[`${target}`]);
-        let classNameLength:number = this.spyClass[`${target}`].length
-        let activeRemovedLength:number = classNameLength - 7 // 7 === "-active".length
-        console.log(classNameLength);
-        let updatedClassName: string = this.spyClass[`${target}`].substring(0,activeRemovedLength)
-        console.log(`updatedClassName: ${updatedClassName}`);
-        this.spyClass[`${eventTarget}`] = updatedClassName;
-
+            this.spyClass[`${eventTarget}`] = inactiveSection;
       }
 
+      if (!this.scrolledTargets.includes(eventTarget) &&
+          this.spyClass[`${eventTarget}`] ==='paragraph-circle-active' ) {
 
-
+            this.spyClass[`${eventTarget}`] = inactiveParagraph;
+      }
     })
-
-      /**
-       * If intersected target is out of view and the viewport is above the id(boundY > 0)
-       * instead of below(user still reading down), then remove "-active" from the targets
-       * class, and remove from the scrolledTargets array
-       */
-      // if (target === "p_circle_3_2") {
-      // console.log('------------------------------------------------------------------');
-      // console.log(target);
-      // console.log(targetStatus[1]);
-      // console.log(boundY);
-      // console.log(ratio);
-      // console.log('------------------------------------------------------------------');
-      // }
-
-      // if (tValue && tValue.length > 17 && !inView && boundY > 0 && ratio < 0.5) {
-      //   // console.log(`HERROOOO : ${tValue}`);
-      //   // console.log(event[0]);
-      //   const newEndIndex = tValue.length - 7;
-      //   const inactiveStyle:string = this.spyClass[`${target}`].substring(0, newEndIndex)
-      //   // const inactiveStyle:string = activeStyle.substring(0, newEndIndex)
-      //   // console.log(inactiveStyle);
-
-      //   this.spyClass[`${target}`] = inactiveStyle;
-
-
-      // }
-    //   console.log(`TARGET: ${eventTarget}`);
-    //   console.log(this.scrolledTargets);
-
-    //   this.scrolledTargets.forEach((target) => {
-    //     const tLength = target.length;
-    //     const tValue = this.spyClass[`${target}`];
-    //   if (tValue && tValue.length > 17 && !this.scrolledTargets.includes(target)) {
-    //     console.log("WHHHHAAAMMMMOOO");
-
-    //     const newEndIndex = tValue.length - 5;
-    //     const inactiveStyle:string = this.spyClass[`${target}`].substring(0, newEndIndex)
-    //     console.log(`INACTIVE STYLE: ${inactiveStyle}`);
-
-    //   }
-    // })
 
 
   }
